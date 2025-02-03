dcfdf24e508a71847559e48eda1ab0598164cc91
kjbrown24 <kjbrown@willamette.edu>
2025-02-03 06:44:02 -0800

    Updated

--- game.py
+++ game.py
@@ -7,9 +7,14 @@ class Game:
     def __init__(self, player1: Character, player2: Character):
         """Initializes the game with two players."""
         self. __player1 = player1
-        
+        self. __player2 = player2
 
+    def attack (self, attacker: Character, defender: Character):
+        """ Performs an attack where the attacker rolls a die to determine damage dealt."""
         pass  #TODO: Implement die roll )1-6) and apply scaled attack power to defender
+    def start_battle(self):
+        """Starts a turn based battle between two players."""
+        pass #TODO: implement the battle loop wehre players take turns attacking
 
 
 
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from game import Game
+from character import Character
+from charactertype import CharacterType
 
+#Create characters
+alice = Character(name="Alice" , character_type=CharacterType.WARRIOR, health=100, attack)
+bob = Character(name="Bob" , character_type=CharacterType.MAGE, health=70, attack_powers=)
+
+#Start game
+game = Game (alice, bob)
+game . start_battle()
